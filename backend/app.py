51f9f27a045d15b83df10164ff77e4bf50785127
Godstaf <chaursiakanishk666@gmail.com>
2026-01-07 14:33:28 +0530

    feat: Implement user registration endpoint and integrate it into the main application.

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from login import router as auth_router
+from register import router as register_router
 
 app = FastAPI()
 
 app.include_router(auth_router)
+app.include_router(register_router)
 
 @app.get("/")
 def read_root():
